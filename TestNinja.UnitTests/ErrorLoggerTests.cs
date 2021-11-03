72bf4e7bf35ec753ccaf0e56416fa38056c3ca3b
olonyl <olony.rocha@contollo.net>
2021-11-03 09:34:40 -0600

    Asserting after subscribing to an Event

--- TestNinja.UnitTests/ErrorLoggerTests.cs
+++ TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -31,5 +31,18 @@ namespace TestNinja.UnitTests
             //Assert.That(() => logger.Log(error), Throws.Exception.TypeOf<ArgumentNullException>());
             Assert.Throws<ArgumentNullException>(() => logger.Log(error));
         }
+
+        [Test]
+        public void Log_ValidError_RaiseErrorLoggedEvent()
+        {
+            var logger = new ErrorLogger();
+            
+            var id = Guid.Empty;
+            logger.ErrorLogged += (sender, args) => { id = args; };
+
+            logger.Log("a");
+
+            Assert.That(id, Is.Not.EqualTo(Guid.Empty));
+        }
     }
 }
