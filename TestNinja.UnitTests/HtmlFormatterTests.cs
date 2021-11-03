96ed3ef3cb4f8796d917f56f82fe4dfbfdebba3d
olonyl <olony.rocha@contollo.net>
2021-11-03 08:28:20 -0600

    Specific and General Assertions

---
+++ TestNinja.UnitTests/HtmlFormatterTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    public class HtmlFormatterTests
+    {
+        [Test]
+        public void FormatAsBold_WhenCalled_ShouldEncloseTheStringWithStrongElement()
+        {
+            var formatter = new HtmlFormatter();
+
+            var result = formatter.FormatAsBold("abc");
+
+            //Specific - disable case sensitive
+            Assert.That(result, Is.EqualTo("<strong>abc</strong>").IgnoreCase);
+
+            //More general
+            //Assert.That(result, Does.StartWith("<strong>").IgnoreCase);
+            //Assert.That(result, Does.EndWith("</strong>").IgnoreCase);
+            //Assert.That(result, Does.Contain("abc"));
+        }
+    }
+}
