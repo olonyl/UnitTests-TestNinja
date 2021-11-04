c4ba974e93b925b0075586dd7cfe8c5a8fd0f38c
olonyl <olony.rocha@contollo.net>
2021-11-04 11:44:02 -0600

    Demerit Point Excersice

---
+++ TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    class DemeritPointsCalculatorTests
+    {
+        [TestCase(-1)]
+        [TestCase(301)]
+        public void CalculateDemeritPoints_SpeedIsNegative_ReturnsArgumentOutOfRangeException(int speed)
+        {
+            var demerit = new DemeritPointsCalculator();
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => demerit.CalculateDemeritPoints(speed));
+        }
+
+        [TestCase(0, 0)]
+        [TestCase(64, 0)]
+        [TestCase(65, 0)]
+        [TestCase(66, 0)]
+        [TestCase(70, 1)]
+        [TestCase(75, 2)]
+        public void CalculateDemeritPoints_WhenCalled_ReturnDemeritPoints(int speed, int expectedResult)
+        {
+            var demerit = new DemeritPointsCalculator();
+
+            var result = demerit.CalculateDemeritPoints(speed);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+    }
+}
--- TestNinja/Fundamentals/DemeritPointsCalculator.cs
+++ TestNinja/Fundamentals/DemeritPointsCalculator.cs
@@ -18,6 +18,6 @@ namespace TestNinja.Fundamentals
             var demeritPoints = (speed - SpeedLimit)/kmPerDemeritPoint;
 
             return demeritPoints;
-        }        
+        }
     }
 }
